cfed7ee006ba501c0a68a5768eb6fdbf08262475
tanglang <976854315@qq.cin>
2015-07-24 10:26:58 +0800

    如何获取ImageView的ImageId

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -4,14 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+	private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = (ImageView) this.findViewById(R.id.image);
+        
+        image.setImageResource(R.drawable.gameback);
+        image.setTag(R.drawable.gameback);
+        
+        image.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if((Integer)image.getTag() == R.drawable.gameback){
+					image.setImageResource(R.drawable.welcomimage);
+					image.setTag(R.drawable.welcomimage);
+				}else{
+					image.setImageResource(R.drawable.gameback);
+			        image.setTag(R.drawable.gameback);
+				}
+			}
+		});
     }
 
 
